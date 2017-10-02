c63eca7ae5cdca9506a08fcdaef101611c81add3
Blproctor <Brittany.l.proctor@gmail.com>
2017-10-02 14:58:49 -0400

    Create ui.js file for events
    Add functions for event crud action successes and failures in ui.js.

---
+++ assets/scripts/events/ui.js
@@ -0,0 +1,56 @@
+'use strict'
+
+// const app = require('../app.js')
+
+const createEventSuccess = function () {
+  console.log('You successfully created a event')
+}
+
+const createEventError = function (response) {
+  console.error(response)
+}
+
+const updateEventSuccess = function () {
+  console.log('you successfully updated an event!')
+}
+
+const updateEventError = function () {
+  console.log('your update failed')
+}
+
+const getAllEventsSuccess = function (data) {
+  console.log('passing through ui.js')
+}
+
+const getAllEventsError = function (response) {
+  console.error(response)
+}
+
+const deleteEventSuccess = function (data) {
+  console.log(data)
+}
+
+const deleteEventError = function (error) {
+  console.error(error)
+}
+
+const getEventSuccess = function (data) {
+  console.log(data)
+}
+
+const getEventError = function (error) {
+  console.error(error)
+}
+
+module.exports = {
+  createEventSuccess,
+  createEventError,
+  updateEventSuccess,
+  updateEventError,
+  getAllEventsSuccess,
+  getAllEventsError,
+  deleteEventSuccess,
+  deleteEventError,
+  getEventSuccess,
+  getEventError
+}
